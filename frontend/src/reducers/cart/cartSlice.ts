fe5d657ca4aa4c7e22e622fb0df517b9a1381c76
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2023-06-06 21:09:07 +0100

    feat: project set-up

--- frontend/src/reducers/cart/cartSlice.ts
+++ frontend/src/reducers/cart/cartSlice.ts
@@ -3,7 +3,7 @@ import { toast } from "react-toastify";
 
 const initialState = {
   cartItems: localStorage.getItem("cartItems")
-    ? JSON.parse(localStorage.getItem("cartItems"))
+    ? JSON.parse(localStorage.getItem("cartItems") as string)
     : [],
   cartTotalQuantity: 0,
   cartTotalAmount: 0,
