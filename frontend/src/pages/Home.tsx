e4888f2692948e284d55f7fd81ca703f8b3d40e9
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2023-06-06 21:59:00 +0100

    feat: project set-up

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -27,7 +27,7 @@ const Home = () => {
               products?.map((product) => (
                 <div key={product.id} className="product">
                   <h3>{product.name}</h3>
-                  <img src={product.image} alt={product.name} />
+                  <img className="w-[300px]" src={product.image} alt={product.name} />
                   <div className="details">
                     <span>{product.desc}</span>
                     <span className="price">${product.price}</span>
