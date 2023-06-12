e4627d6efdbb903dada6b4c71080249fa4b0aa9b
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2023-06-12 14:30:51 +0100

    feat: product_frame  build

--- frontend/src/components/Home/FeaturedProducts.tsx
+++ frontend/src/components/Home/FeaturedProducts.tsx
@@ -1,33 +1,14 @@
-import { useAddQuantity, useAddToCart, useDecreaseQuantity } from '../hooks/useCartControls';
-import { useAppSelector } from '../../network/hooks';
-import { RootState } from '../../network/store';
 import { Navigation } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import { CartItem, Product } from '../../types';
+import { Product } from '../../types';
 import { selectedProductsArray } from '../../utils/data';
-import { Minus, Plus, ShoppingCartSimple } from '@phosphor-icons/react';
-import { Link } from 'react-router-dom';
-import QuantityControlsBtn from '../products/QuantityControlsBtn';
-import { motion } from 'framer-motion';
-import { useState } from 'react';
 import ProductFrame from '../products/ProductFrame';
 
 
 
-
 const SelectedProducts = () => {
-  const cart = useAppSelector((state: RootState) => state.cart);
-  const addToCart = useAddToCart();
-  const addQuantity = useAddQuantity();
-  const decreaseQuantity = useDecreaseQuantity();
-
-  const [animationKey, setAnimationKey] = useState<number>(0);
-
-const animateQuantityCount = () => {
-  setAnimationKey((prevKey) => prevKey + 1);
-};
 
   return (
     <section className='pt-44 pb-20'>
@@ -54,48 +35,10 @@ const animateQuantityCount = () => {
           }}
         >
           <h1 className="absolute top-0 left-0 text-4xl font-nunitosans font-bold leading-tight">Featured products</h1>
-          {selectedProductsArray.map((val: Product, i: number) => {
-            // const existingCartItem = cart.cartItems.find((item: CartItem) => item.id === val.id);
+          {selectedProductsArray.map((product: Product, i: number) => {
             return (
               <SwiperSlide key={i}>
-                <ProductFrame val={val} />
-                {/* <div key={val.id} className='mt-40px'>
-                  <Link to="/product/product-details">
-                    <img className='rounded-[5px]' src={val.image} alt={'enchanté_fashon'} />
-                  </Link>
-                  <div className='flex justify-between pt-[14px]'>
-                    <div className="details">
-                      <h3 className='font-medium capitalize'>{val.name}</h3>
-                      <div className='flex items-start space-x-3'>
-                        <span className="text-[24px] font-medium montserrat">${val.price}</span>
-                        {val.discount && <span className='text-[16px] font-medium montserrat opacity-60 discount-strike'>${(val.price * 0.3) + val.price}</span>}
-                      </div>
-                    </div>
-                    <div>
-                      {existingCartItem ? (
-                        <div className='flex items-center space-x-4'>
-                          <QuantityControlsBtn
-                            onClick={() => { decreaseQuantity(existingCartItem); animateQuantityCount(); }}
-                            children={<Minus size={22} color="#f8f8f8" weight="bold" />}
-                            className='rounded-[5px] hover:opacity-70 p-2 bg-black text-white'
-                          />
-
-                          <motion.span key={animationKey} animate={{ scale: [1.3, 1] }} className='font-bold'>{existingCartItem.cartQuantity}</motion.span>
-
-                          <QuantityControlsBtn
-                             onClick={() => { addQuantity(existingCartItem); animateQuantityCount(); }}
-                            children={<Plus size={22} color="#f8f8f8" weight="bold" />}
-                            className='rounded-[5px] hover:opacity-70 p-2 bg-black text-white'
-                          />
-                        </div>
-                      ) : (
-                        <button className="rounded-[5px] hover:opacity-70 transition duration-300 p-3 bg-black text-white" onClick={() => addToCart(val)}>
-                          <ShoppingCartSimple size={22} color="#f8f8f8" weight="regular" />
-                        </button>
-                      )}
-                    </div>
-                  </div>
-                </div> */}
+                <ProductFrame product={product} />
               </SwiperSlide>
             );
           })}
--- frontend/src/components/products/ProductFrame.tsx
+++ frontend/src/components/products/ProductFrame.tsx
@@ -1,12 +1,9 @@
 import { useAddQuantity, useAddToCart, useDecreaseQuantity } from '../hooks/useCartControls';
 import { useAppSelector } from '../../network/hooks';
 import { RootState } from '../../network/store';
-import { Navigation } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { CartItem, Product } from '../../types';
-import { selectedProductsArray } from '../../utils/data';
 import { Minus, Plus, ShoppingCartSimple } from '@phosphor-icons/react';
 import { Link } from 'react-router-dom';
 import QuantityControlsBtn from './QuantityControlsBtn';
@@ -15,45 +12,37 @@ import { useState } from 'react';
 
 
 interface Props {
-    val: Product
+    product: Product
 }
 
-const ProductFrame = ({val}: Props) => {
-
+const ProductFrame = ({ product }: Props) => {
     const cart = useAppSelector((state: RootState) => state.cart);
     const addToCart = useAddToCart();
     const addQuantity = useAddQuantity();
     const decreaseQuantity = useDecreaseQuantity();
-
-    const existingCartItem = cart.cartItems.find((item: CartItem) => item.id === val.id);
-
     const [animationKey, setAnimationKey] = useState<number>(0);
-
-    const animateQuantityCount = () => {
-      setAnimationKey((prevKey) => prevKey + 1);
-    };
-    
+    const existingCartItem = cart.cartItems.find((item: CartItem) => item.id === product.id);
 
 
     return (
 
-        <div key={val.id} className='mt-40px'>
+        <div key={product.id} className=''>
             <Link to="/product/product-details">
-                <img className='rounded-[5px]' src={val.image} alt={'enchanté_fashon'} />
+                <img className='rounded-[5px]' src={product.image} alt={'enchanté_fashon'} />
             </Link>
             <div className='flex justify-between pt-[14px]'>
                 <div className="details">
-                    <h3 className='font-medium capitalize'>{val.name}</h3>
+                    <h3 className='font-medium capitalize'>{product.name}</h3>
                     <div className='flex items-start space-x-3'>
-                        <span className="text-[24px] font-medium montserrat">${val.price}</span>
-                        {val.discount && <span className='text-[16px] font-medium montserrat opacity-60 discount-strike'>${(val.price * 0.3) + val.price}</span>}
+                        <span className="text-[24px] font-medium montserrat">${product.price}</span>
+                        {product.discount && <span className='text-[16px] font-medium montserrat opacity-60 discount-strike'>${(product.price * 0.3) + product.price}</span>}
                     </div>
                 </div>
                 <div>
                     {existingCartItem ? (
                         <div className='flex items-center space-x-4'>
                             <QuantityControlsBtn
-                                onClick={() => { decreaseQuantity(existingCartItem); animateQuantityCount(); }}
+                                onClick={() => { decreaseQuantity(existingCartItem); setAnimationKey((prevKey) => prevKey + 1) }}
                                 children={<Minus size={22} color="#f8f8f8" weight="bold" />}
                                 className='rounded-[5px] hover:opacity-70 p-2 bg-black text-white'
                             />
@@ -61,13 +50,13 @@ const ProductFrame = ({val}: Props) => {
                             <motion.span key={animationKey} animate={{ scale: [1.3, 1] }} className='font-bold'>{existingCartItem.cartQuantity}</motion.span>
 
                             <QuantityControlsBtn
-                                onClick={() => { addQuantity(existingCartItem); animateQuantityCount(); }}
+                                onClick={() => { addQuantity(existingCartItem); setAnimationKey((prevKey) => prevKey + 1) }}
                                 children={<Plus size={22} color="#f8f8f8" weight="bold" />}
                                 className='rounded-[5px] hover:opacity-70 p-2 bg-black text-white'
                             />
                         </div>
                     ) : (
-                        <button className="rounded-[5px] hover:opacity-70 transition duration-300 p-3 bg-black text-white" onClick={() => addToCart(val)}>
+                        <button className="rounded-[5px] hover:opacity-70 transition duration-300 p-3 bg-black text-white" onClick={() => addToCart(product)}>
                             <ShoppingCartSimple size={22} color="#f8f8f8" weight="regular" />
                         </button>
                     )}
