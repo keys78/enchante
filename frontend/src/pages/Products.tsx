4afedf927846ffbb50673d88c7309ac671855e4a
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2023-06-14 05:24:10 +0100

    feat:products_page_ui build/fixes

--- frontend/src/pages/Products.tsx
+++ frontend/src/pages/Products.tsx
@@ -211,7 +211,7 @@ const Products = () => {
                             {Object.keys(filterTerms).length > 0 && <h3 className="text-gray-400">Applied Filters:</h3>}
                             {Object.entries(filterTerms).map(([key, value], index) => (
                                 <div
-                                    className='opacity-60 px-2 py-1 border rounded-[20px]'
+                                    className='opacity-60 px-2 py-1 border rounded-[20px] lowercase'
                                     key={key}
                                     style={{ backgroundColor: `rgba(300, 300, 300, ${0.8 - (index + 1) * 0.1})` }}
                                 >
@@ -219,20 +219,12 @@ const Products = () => {
                                     {key === 'color' && value !== null && <span className="text-gray-500">{value}</span>}
                                     {key === 'brand' && value !== null && <span className="text-gray-500">{value}</span>}
                                     {key === 'price' && value !== null && <span className="text-gray-500">{`prices below: ${Number(value) + 1}`}</span>}
-                                    {/* {key === 'starNumberOfRatings' && value !== null && <span className="text-gray-500">{`star(s): ${value}`}</span>} */}
-                                    {key === 'starNumberOfRatings' && value !== null && (
-  <span className="text-gray-500"> {`${value === "1" ? '1 star' : `${value} stars`}`} </span>)}
-                                    {key === 'freeShipping' && value === "true" && <span className="text-gray-500"> Free Shipping</span>}
-                                    {key === 'newProduct' && value === "true" && <span className="text-gray-500"> New Product</span>}
+                                    {key === 'starNumberOfRatings' && value !== null && (<span className="text-gray-500"> {`${value === "1" ? '1 star' : `${value} stars`}`} </span>)}
+                                    {key === 'freeShipping' && value === "true" && <span className="text-gray-500"> free shipping</span>}
+                                    {key === 'newProduct' && value === "true" && <span className="text-gray-500"> new product</span>}
                                 </div>
                             ))}
                         </div>
-
-
-
-
-
-
 
 
 
@@ -247,7 +239,7 @@ const Products = () => {
                                 </div>
                             </>
                         ) : (
-                            <p>No products available.</p>
+                            <p className='flex items-center justify-center h-[300px]'>No products available.</p>
                         )}
                     </div>
                 </div>
