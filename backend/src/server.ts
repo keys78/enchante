df5ee3c54b0361aff6bf580198fa4a81be160283
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2023-08-02 23:13:35 +0100

    feat: docker setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,8 +4,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await  mongoose.connect('mongodb://localhost:27017/enchante')
-    // await mongoose.connect(process.env.MONGO_URI)
+    // await  mongoose.connect('mongodb://localhost:27017/enchante')
+    await mongoose.connect(process.env.MONGO_URI)
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
